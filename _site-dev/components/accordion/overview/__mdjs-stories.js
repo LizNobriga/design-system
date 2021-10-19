fbd452e55413c83f53dba92dde76ccf44699ee5e
Aaron Hans <aaronhans@gmail.com>
2021-10-19 09:14:03 -0700

    integrated one component

---
+++ _site-dev/components/accordion/overview/__mdjs-stories.js
@@ -0,0 +1,45 @@
+/** script code **/
+import '../../../../components/accordion/dist/index.js';
+import '../../../../components/plus/index.js';
+import '../../../../components/minus/index.js';
+import { html } from 'lit-html';
+/** stories code **/
+export const demo = () => html`
+  <cagov-accordion>
+    <div class="cagov-accordion-card">
+      <button class="accordion-card-header accordion-alpha" type="button" aria-expanded="false">
+        <div class="accordion-title">Lorem ipsum</div>
+        <div class="plus-minus">
+          <cagov-plus></cagov-plus>
+          <cagov-minus></cagov-minus>
+        </div>
+      </button>
+      <div class="accordion-card-container" aria-hidden=" true" style="height: 0px;">
+        <div class="card-body">
+          <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+            moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+            Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+            shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+            proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+            aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+        </div>
+      </div>
+    </div>
+  </cagov-accordion>`;
+/** stories setup code **/
+const rootNode = document;
+const stories = [{ key: 'demo', story: demo }];
+let needsMdjsElements = false;
+for (const story of stories) {
+  const storyEl = rootNode.querySelector(`[mdjs-story-name="${story.key}"]`);
+  if (storyEl) {
+    storyEl.story = story.story;
+    storyEl.key = story.key;
+    needsMdjsElements = true;
+    Object.assign(storyEl, {});
+  }
+};
+if (needsMdjsElements) {
+  if (!customElements.get('mdjs-preview')) { import('@mdjs/mdjs-preview/define'); }
+  if (!customElements.get('mdjs-story')) { import('@mdjs/mdjs-story/define'); }
+}
---
+++ docs/components/accordion/use-cases.md
@@ -0,0 +1,19 @@
+# Accordion >> Use cases
+
+## Open/closed
+
+### Starts closed
+
+### Specify open to start opened
+
+## Used on
+
+### covid19.ca.gov
+
+### alpha.ca.gov
+
+<img src="https://github.com/cagov/storybook-ca-gov/raw/main/packages/%40cagov/accordion/stories/accordion.png" />
+
+
+
+
